1b4bbcd9051e123336ee732e0d8dda969fa8cf9c
DuskyShelf <dupsdale@gmail.com>
2015-09-23 17:44:43 +0100

    added Player and Team classes

---
+++ spec/player_spec.rb
@@ -0,0 +1,20 @@
+require 'player'
+require 'team'
+
+describe Player do
+
+  describe 'applying for a team' do
+
+    let(:player) { Player.new :female }
+    let(:team) { Team.new }
+
+    it 'is valid if the player is not already part of a team' do
+      expect(player).to be_hireable_player
+    end
+
+    it 'is not valid if the player is already part of a team' do
+      team.add_player player
+      expect(player).not_to be_hireable_player
+    end
+  end
+end
